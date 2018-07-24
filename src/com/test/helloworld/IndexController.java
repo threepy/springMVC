ce632cb37e0d9851757c98bef27621871641f9a0
threepy <threepy@github.com>
2018-07-24 13:35:05 +0800

    test log4j2 log

--- src/com/test/helloworld/IndexController.java
+++ src/com/test/helloworld/IndexController.java
@@ -1,22 +1,26 @@
 package com.test.helloworld;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.logging.Logger;
-
 @Controller
 @RequestMapping("/index")
 public class IndexController {
-    private static final Logger logger = Logger.getLogger(IndexController.class.getName());
+    private static final Logger logger = LogManager.getLogger(IndexController.class);
 
     @RequestMapping("/")
     public String say(ModelMap model) {
         model.addAttribute("title", "this is title");
         model.addAttribute("end", "the end");
-        logger.info("请求index页面");
+        logger.error("this is error log");
+        logger.info("this is info log");
+        logger.warn("this is info log");
+        logger.trace("this trace log");
+        logger.debug("this debug log");
         return "index";
     }
 
--- src/com/test/helloworld/TestController.java
+++ src/com/test/helloworld/TestController.java
@@ -1,15 +1,20 @@
 package com.test.helloworld;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("test")
+@RequestMapping("/test")
 public class TestController {
+    private static final Logger logger = LogManager.getLogger(TestController.class);
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(ModelMap model) {
-        return "test";
+        logger.info("this TestController info log");
+        return "final";
     }
 }
